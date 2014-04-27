175eda2a98a1d925a7ebfbeb4616b22bdf0c1552
sky <diufanshu@gmail.com>
2014-04-27 12:33:18 +0800

    处理时间函数 done!

--- graduation/codes/debag.py
+++ graduation/codes/debag.py
@@ -1,14 +1,25 @@
+def processtime(input_data):
+	data = input_data.split('\n')
+	first_line = data[0].split()
+	n = int(first_line[0])
+	m = int(first_line[1])
+	q = []
+	m = len(data)
+	for i in xrange(1,m):
+		q.append(int(data[i]))
+	print q
+	print n
+	print 'data loaded'
+	c = [None]*n
+	d= []
+	for k in xrange(n):
+		c[k] = (k+1)*q[0]
 
-
-
-
-
-
-
-
-
-
-
+	for i in xrange(1,m-1):
+		c[0] = c[0] + q[i]
+		for k in xrange(1,n):
+			c[k] = max(c[k],c[k-1])+q[i]
+	return c[-1]
 
 
 
@@ -19,5 +30,5 @@ if __name__ == '__main__':
 		input_data_file = open(file_location, 'r')
 		input_data = ''.join(input_data_file.readlines())
 		input_data_file.close()
-		a = input_data.split('\n')
-		print a
+		c = processtime(input_data)
+		print c
